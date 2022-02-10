6f8a9252572d40615c1b15f6a97ecedb9437d655
serafla <63385649+serafla@users.noreply.github.com>
2022-02-09 22:02:53 -0500

    finished simple nav

--- dev/js/pageScroll.js
+++ dev/js/pageScroll.js
@@ -3,7 +3,7 @@ import { ScrollToPlugin } from "gsap/ScrollToPlugin";
 
 gsap.registerPlugin(ScrollToPlugin);
 
-let idArray = ["#Introduction","#Branding","#Web Design", "#Typography"];
+let idArray = ["#Introduction","#Branding","#WebDesign", "#Typography"];
 
 export function scrollPage(index){
     gsap.to(window, {duration: 1, scrollTo:idArray[index]});
